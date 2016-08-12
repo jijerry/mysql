7fc62d8f59178d200e7a676b4b2c5cbf6d670f03
Jerry <938334866@qq.com>
2016-08-12 20:05:30 +0800

    数据库添加，修改完成

--- sqltest.php
+++ sqltest.php
@@ -12,8 +12,27 @@ if (!$db_server) die("Unable to conncet to MYSQL".mysqli_error($db_server));
 mysqli_select_db($db_server, $db_database)
     or die("Unable to choose DATABASE".mysqli_error($db_server));
 
+if (isset($_POST['delete'])&&isset($_POST['id']))
+{
+    $id = $_POST['id'];
+    $query = "DELETE FROM classtable WHERE  id = '$id'";
+    if (!mysqli_query($db_server, $query)){
+        echo "DELETE data is failed".mysqli_error($db_server);
+    }
+}
+if (isset($_POST['id'])&& isset($_POST['name']))
+{
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    echo $id, $name;
+    $query = "INSERT INTO classtable(id,name)VALUES('$id','$name')";
+    if (!mysqli_query($db_server, $query)){
+        echo "INSERT data is failed".mysqli_error($db_server);
+    }
+}
+
 echo <<<_END
-<form action="sqltest.php method="post"><pre>
+<form action="sqltest.php" method="post"><pre>
 id <input type ="text" name="id"/>
 name <input type ="text" name = "name"/>
     <input type = "submit" value ="ADD RECORD"/>
@@ -21,11 +40,11 @@ name <input type ="text" name = "name"/>
 _END;
 
 
-$query = "SELECT * FROM class";
+$query = "SELECT * FROM classtable";
 $results = mysqli_query($db_server, $query);
 if (!$results) die("Database access failed".mysqli_error($db_server));
 $rows = mysqli_num_rows($results);
-for ($row = 0; $row < $rows; $row++) {
+for ($j = 0; $j < $rows; $j++) {
     $row = mysqli_fetch_row($results);
     echo <<<_END
     <pre>
@@ -35,7 +54,7 @@ for ($row = 0; $row < $rows; $row++) {
 
 <form action = "sqltest.php" method = "post"><pre>
 <input type = "hidden" name = "delete" value = "yes"/>
-<input type = "hidden" name = "usename" value = "$row[0]"/>
+<input type = "hidden" name = "id" value = "$row[0]"/>
 <input type = "submit" value = "DELETE RECORD"/>
 </pre></form>
 
